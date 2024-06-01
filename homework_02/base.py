e782191d8c8918fdf531694642a0f3c1d5d3befc
A.Sedov <A.Sedov@x5.ru>
2024-06-01 20:13:29 +0300

    половина

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,9 +1,27 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    def __init__(self, weight=1500, started=False, fuel=92, fuel_consumption=5):
+    def __init__(self, weight=1500, started=False, fuel=150, fuel_consumption=5):
         self.weight = weight
         self.started = started
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
+                print("Автомобиль заведён")
+            else:
+                raise LowFuelError("Нет топлива, необходимо заправиться")
+
+    def move(self, distance):
+        if self.started:
+            required_fuel = distance * self.fuel_consumption
+            if required_fuel <= self.fuel:
+                self.fuel -= required_fuel
+                print(f"Машина проедет {distance} км")
+            else:
+                raise NotEnoughFuel("Not enough fuel to cover the distance.")
